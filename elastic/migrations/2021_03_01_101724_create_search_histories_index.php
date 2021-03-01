1063f16fefda216d1ecdd637c431756201b690f5
ohmycthulhu <elvin.bayramov@protonmail.com>
2021-03-01 14:18:33 +0400

    Search autocomplete system

---
+++ elastic/migrations/2021_03_01_101724_create_search_histories_index.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use ElasticAdapter\Indices\Mapping;
+use ElasticAdapter\Indices\Settings;
+use ElasticMigrations\Facades\Index;
+use ElasticMigrations\MigrationInterface;
+
+final class CreateSearchHistoriesIndex implements MigrationInterface
+{
+    /**
+     * Run the migration.
+     */
+    public function up(): void
+    {
+      Index::create('search_histories', function (Mapping $mapping, Settings $settings) {
+        $mapping->text('text');
+        $mapping->keyword('weight');
+      });
+    }
+
+    /**
+     * Reverse the migration.
+     */
+    public function down(): void
+    {
+      Index::dropIfExists('search_histories');
+    }
+}
