2217364ae60bbc58bc0c2cb761e262dcb1f9107f
Elvin <fruiteatearth@gmail.com>
2021-06-04 02:55:30 +0400

    Bug fix

---
+++ database/migrations/2021_06_03_225420_fix_color_column_in_subways.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixColorColumnInSubways extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subways', function (Blueprint $table) {
+            $table->dropColumn('color');
+            $table->string('color')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subways', function (Blueprint $table) {
+          $table->dropColumn('color');
+          $table->char('color', 9)->nullable();
+        });
+    }
+}
