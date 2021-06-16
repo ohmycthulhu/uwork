3cd3919a05b0539f5285e6cde93d6d562d0e46a2
Elvin <fruiteatearth@gmail.com>
2021-06-16 20:45:46 +0400

    Performance test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+function getTime() {
+  return time() + microtime(true);
+}
+
+$times = [
+  'start' => getTime(),
+];
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
@@ -23,6 +31,8 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
+$times['autoload'] = getTime();
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -36,6 +46,8 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+$times['app'] = getTime();
 
 /*
 |--------------------------------------------------------------------------
@@ -51,10 +63,40 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+$times['kernel'] = getTime();
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
+$times['response'] = getTime();
+
 $response->send();
 
-$kernel->terminate($request, $response);
+$times['sending'] = getTime();
+
+//$kernel->terminate($request, $response);
+
+$times['terminating'] = getTime();
+$keys = array_keys($times);
+
+$diffs = array_reduce(
+  array_keys($keys),
+  function ($arr, $index) use ($times, $keys) {
+    if ($index === 0) {
+      $val = 0;
+    } else {
+      $val = $times[$keys[$index]] - $times[$keys[$index - 1]];
+    }
+    $arr[$keys[$index]] = $val;
+    return $arr;
+  },
+  []
+);
+
+if (false) {
+  echo "<br>";
+  foreach ($diffs as $type => $time) {
+    echo "$type - $time<br>";
+  }
+}
